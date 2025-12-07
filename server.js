5d2fd240c7d2fd6406b581a90ac596581570c2f1
hazal delal <hazaldelalbasut@gmail.com>
2025-12-08 00:34:10 +0300

    bunu nereye ekliceğimi bilemedim aşağı attım

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+
+
+const veriler = require('./public/js/simulasyon_verileri.js'); 
+
+
+app.set('view engine', 'ejs'); 
+app.set('views', path.join(__dirname, 'views'));
+
+
+app.use(express.static('public')); 
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.redirect('/notes.html');
+});
+
+
+app.get('/notes.html', (req, res) => {
+    res.render('notes', { 
+        aktifKullanici: veriler.aktifKullanici,
+        notlarListesi: veriler.notlarListesi 
+    });
+});
+
+app.get('/messaging.html', (req, res) => {
+    res.render('messaging', { 
+        aktifKullanici: veriler.aktifKullanici,
+        sohbetVerileri: veriler.sohbetVerileri 
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Sunucu http://localhost:${port} adresinde çalışıyor...`);
+});
