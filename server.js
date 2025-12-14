aaf76ab43636a731a10adca6d4069269973ef55f
minelceren <guler.minelceren@ogr.ahievran.edu.tr>
2025-12-14 19:46:00 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = 3000;
 const path = require('path');
 
 
-const veriler = require('./public/js/simulasyon_verileri.js'); 
+const veriler = require('../public/js/simulasyon_verileri.js'); 
 
 
 app.set('view engine', 'ejs'); 
