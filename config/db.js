ccc628158cc54e7a0c9125b55b450c1abc5f4277
berfin-ozger <aysenberfinozger@gmail.com>
2025-12-15 01:36:24 +0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -8,7 +8,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: '31415',
+  password: 'brfn2005',
   database: 'OBS',
   waitForConnections: true,
   connectionLimit: 10,
